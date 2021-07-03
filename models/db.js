f9ba9b637f4639759de85a5d8d4c1cf19e7c0a2d
Thanh Dat <datptgcc18100@fpt.edu.vn>
2021-07-03 23:56:54 +0700

    first commit

---
+++ models/db.js
@@ -0,0 +1,11 @@
+
+const mongoose = require('mongoose');
+const url = "mongodb+srv://phanvandat:phanvandat123@cluster0.8vzkn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+mongoose.connect(url,{useNewUrlParser:true},(err) => {
+    if(!err){ console.log("MongoDB Connection Succeeded");}
+    else{
+        console.log("An Error Occured");
+    } 
+})
+
+require('./product.model');
